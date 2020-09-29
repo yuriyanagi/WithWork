5312007c2fd36ca2e8654c7395032bfec43c6de8
yuriyanagi <40306yy@gmail.com>
2020-09-29 23:49:35 +0900

    Update records_controller.rb

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,4 +1,19 @@
 class RecordsController < ApplicationController
+
   def index
+    @records = Record.all
+    @record = Record.new
+  end
+
+  def create
+    @record = Record.new(record_params)
+    @record.save
+  end
+
+  private
+
+  def record_params
+    params[:record]
+    params.require(:record).permit(:date, :start_time, :end_time, :class1, :class2, :class3)
   end
 end
