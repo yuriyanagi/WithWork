1337fd86aa85ab35c14e8ee9ed276476c7cae2c3
yuriyanagi <40306yy@gmail.com>
2020-10-07 18:10:46 +0900

    create projects_create

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,4 +10,20 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to projects_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def project_params
+    params[:project].permit(:title)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "records#index"
   resources :records, only: [:index, :create, :edit, :update]
-  resources :projects, only: [:index, :show, :new]
+  resources :projects, only: [:index, :show, :new, :create]
 end
