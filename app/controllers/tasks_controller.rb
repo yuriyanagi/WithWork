fdc6595bce9d9d12bcc0dac986bf4bf379e0006f
yuriyanagi <40306yy@gmail.com>
2020-10-08 00:36:46 +0900

    create task_create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,13 @@
 class TasksController < ApplicationController
+  def create
+    @project = Project.find(params[:project_id])
+    @task    = @project.tasks.create(tasks_params)
+    redirect_to project_path(@project.id)
+  end
+
+  private
+
+  def tasks_params
+    params[:task].permit(:title, :deadline, :remarks)
+  end
 end
