ad946f4f1bd403f6d74e1a0b43d7c839a75a6dc4
yuriyanagi <40306yy@gmail.com>
2020-10-07 19:36:04 +0900

    create projects_destroy

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -33,6 +33,12 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path
+  end
+
   private
 
   def project_params
