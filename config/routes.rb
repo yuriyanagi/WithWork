81fbc06c5a03018083e704bc813a5abe1fefe8b6
yuriyanagi <40306yy@gmail.com>
2020-10-08 00:17:00 +0900

    create task_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "records#index"
   resources :records, only: [:index, :create, :edit, :update]
-  resources :projects, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :projects do
+    resources :tasks, only: [:show]
+  end
 end
