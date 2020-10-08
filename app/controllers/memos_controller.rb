d6551c94a86dfde4766506450371ad5e01a5043a
yuriyanagi <40306yy@gmail.com>
2020-10-08 10:02:12 +0900

    create memo_new

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -2,4 +2,8 @@ class MemosController < ApplicationController
   def index
     @memos = Memo.all
   end
+
+  def new
+    @memo = Memo.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :projects do
     resources :tasks, only: [:create, :destroy]
   end
-  resources :memos, only: [:index]
+  resources :memos, only: [:index, :new]
 end
